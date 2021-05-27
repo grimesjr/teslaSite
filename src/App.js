b5a6193de9ecacb404c0133fea81d4740bdbcdb1
Jimmy Lin <jimmy@Jimmys-Mac-mini.local>
2021-05-27 16:14:45 -0700

    added redux for cars

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './App.css';
-import styled from 'styled-components'
 import Header from './components/Header'
 import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-      {/* <Header/> */}
+      <Header/>
       <Home/>
     </div>
   );
